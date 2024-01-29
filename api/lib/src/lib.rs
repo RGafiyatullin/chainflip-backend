7b88d8ab9b3398c0d994ed946a2d424f65bcbe8a
Raman Hafiyatulin <r.gafiyatullin@me.com>
2024-01-29 10:12:34 +0200

    make `cargo build -p chainflip-cli` work

--- api/lib/src/lib.rs
+++ api/lib/src/lib.rs
@@ -364,6 +364,7 @@ pub fn clean_foreign_chain_address(chain: ForeignChain, address: &str) -> Result
 		ForeignChain::Polkadot =>
 			EncodedAddress::Dot(PolkadotAccountId::from_str(address).map(|id| *id.aliased_ref())?),
 		ForeignChain::Bitcoin => EncodedAddress::Btc(address.as_bytes().to_vec()),
+		ForeignChain::Solana => EncodedAddress::Sol(address.as_bytes().try_into()?),
 	})
 }
 
